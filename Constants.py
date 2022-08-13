16efb359a91c08d8ef39aa336dcf636c1c7150a1
Tim Clark <tgclark20@gmail.com>
2022-08-12 22:12:15 -0400

    Added methon and constants for example question 3

---
+++ Constants.py
@@ -0,0 +1,5 @@
+encodings= {'<DEGC>': 'Celcius', '<DEGF>':'Fahrenheit', '<NM>':'Nanometers', '<MM>': 'Millimeters','<CM>':'Centimeters', '<MS>':'Milliseconds', '<S>':'Seconds','<MIN>':'Minutes'}
+timeEncodings=['Milliseconds','Seconds','Minutes']
+measurmentEncodings=['Nanometers','Millimeters','Centimeters']
+tempEncodings=['Celcius','Fahrenheit']
+standardConversions={'Nanometers':.0001,'Millimeters':.001, 'Centimeters':.01, 'Seconds':1.0, 'Minutes':60.0, 'Milliseconds':.001}
--- ExampleQuestions.py
+++ ExampleQuestions.py
@@ -3,19 +3,17 @@
 # Created Date: 08/12/2022
 # Description: Methods requested as coding samples.
 # ---------------------------------------------------------------------------
-testString='''INSTRUMENT_ID = "MDIS-NAC"
-FILTER_NUMBER = "2"
-CENTER_FILTER_WAVELENGTH = 747.7 <NM>
-BANDWIDTH = 52.6 <NM>
-EXPOSURE_DURATION = 192 <MS>
-EXPOSURE_TYPE = AUTO
-DETECTOR_TEMPERATURE = -42.55 <DEGC>
-FOCAL_PLANE_TEMPERATURE = -35.94 <DEGC>
-FILTER_TEMPERATURE = "N/A"
-OPTICS_TEMPERATURE = -37.36 <DEGC>'''
+import Constants
+
+encodings= Constants.encodings
+tempEncodings=Constants.tempEncodings
+timeEncodings=Constants.timeEncodings
+measurementEncodings=Constants.measurmentEncodings
+standardConversions=Constants.standardConversions
 
 # Example Question 1
 def questionOne(inputString, keyword):
+    
     inputDict = dict((x.strip(), y.strip())  
         for x, y in (element.split(' ')  
             for element in inputString.split('\n'))) 
@@ -25,6 +23,7 @@ def questionOne(inputString, keyword):
 
 # Example Question 2
 def questionTwo(inputString, keyword):
+    
     inputString= inputString.replace(' = ',' ')
     inputDict =dict()
     
@@ -36,5 +35,40 @@ def questionTwo(inputString, keyword):
             inputDict[values[0]]={'value': values[1], 'unit': values[2]}
     
     return inputDict[keyword].get('value')
+
+
+#Example Question 3
+def questionThree(inputString, unit ,keyword):
+    inputString= inputString.replace(' = ',' ')
+    inputDict =dict()
+    
+    for element in inputString.split('\n'):
+        values = element.split()
+        if len(values)==2:
+            inputDict[values[0]]= {'value' : values[1]}
+        else:
+            inputDict[values[0]]={'value': values[1], 'unit': values[2]}
+
+    selectedValue=float(inputDict[keyword].get('value'))
+    selectedUnit=inputDict[keyword].get('unit')
+    unitExpanded=encodings[selectedUnit]
+    print(unit, unitExpanded)
+    if unitExpanded in measurementEncodings and unit in measurementEncodings:
+       return (selectedValue*standardConversions[unitExpanded])/(standardConversions[unit])
+
+    elif unitExpanded in timeEncodings and unit in timeEncodings:
+       return (selectedValue*standardConversions[unitExpanded])/(standardConversions[unit])
+    
+    elif unitExpanded in tempEncodings and unit in tempEncodings:
+        if unit == unitExpanded:
+            return selectedValue
+        elif unit == 'Celcius':
+            return round(((5.0/9.0)*(selectedValue-32)),2)
+        elif unit == 'Fahrenheit':
+            return round((((9.0/5.0)*(selectedValue))+32),2)
+        else:
+            return "an Exception has Occurred"
+    else:
+        return "***Unit mismatch***"
 
 
